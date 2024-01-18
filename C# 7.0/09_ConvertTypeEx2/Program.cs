e530d8eb28c71c7bff3f050b6700580cbbbc8d75
SUN <forten99@gmail.com>
2024-01-18 21:58:52 +0900

    C# 7.0 Part1 9차시 ~ 10차시

---
+++ C# 7.0/09_ConvertTypeEx2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_ConvertTypeEx2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 부동 소수점 형식 사이의 형변환
+
+            float f = 79.6777f;
+            Console.WriteLine("f : {0}", f);
+
+            double d = (double)f;
+            Console.WriteLine("d : {0}", d);
+
+            double dd = 0.11212121212121212d;
+            Console.WriteLine("dd : {0}", dd);
+
+            float ff = (float)dd;
+            Console.WriteLine("ff : {0}", ff);
+
+            float x = 69.6875f;
+            Console.WriteLine("x : {0}", x);
+
+            double dx = (double)x;
+            Console.WriteLine("dx : {0}", dx);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C# 7.0/10_ConvertTypeEx3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_ConvertTypeEx3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             부호있는 정수형과 부호없는 정수형 사이의 형변환
+             byte, sbyte
+             
+             int uint, short ushort, long ulong
+             */
+
+            //byte bb = 255;
+
+            //sbyte sb = (sbyte)bb;
+
+            //Console.WriteLine(bb);
+            //Console.WriteLine(sb);
+
+            //Console.ReadKey();
+
+            int ia = 600;
+            Console.WriteLine(ia);
+
+            uint ua = (uint)ia;
+            Console.WriteLine(ua);
+
+
+            int iaa = -40;
+            Console.WriteLine(iaa);
+
+            uint uaa = (uint)iaa;
+            Console.WriteLine(uaa);
+
+            Console.ReadKey();
+        }
+    }
+}
